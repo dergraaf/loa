a407dd967928d8c9c21cd93b208560423f654593
strongly-typed <strongly-typed@nan>
2012-04-30 03:31:17 +0200

    Read and Write functions for testbench introduced.

--- fpga/modules/spislave/hdl/spislave_pkg.vhd
+++ fpga/modules/spislave/hdl/spislave_pkg.vhd
@@ -6,7 +6,7 @@
 -- Author     : cjt@users.sourceforge.net
 -- Company    : 
 -- Created    : 2011-08-27
--- Last update: 2012-04-29
+-- Last update: 2012-04-30
 -- Platform   : 
 -------------------------------------------------------------------------------
 -- Description: 
@@ -16,30 +16,125 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 library work;
 use work.bus_pkg.all;
 
 package spislave_pkg is
-  
-  -----------------------------------------------------------------------------
-  -- Component declarations
-  -----------------------------------------------------------------------------
-  component spi_slave
-    port (
-      miso_p : out std_logic;
-      mosi_p : in  std_logic;
-      sck_p  : in  std_logic;
-      csn_p  : in  std_logic;
 
-      bus_o : out busmaster_out_type;
+   -----------------------------------------------------------------------------
+   -- Component declarations
+   -----------------------------------------------------------------------------
+   component spi_slave
+      port (
+         miso_p : out std_logic;
+         mosi_p : in  std_logic;
+         sck_p  : in  std_logic;
+         csn_p  : in  std_logic;
 
-      bus_i : in busmaster_in_type;
+         bus_o : out busmaster_out_type;
 
-      reset : in std_logic;
-      clk   : in std_logic);
-  end component;
-  
+         bus_i : in busmaster_in_type;
+
+         reset : in std_logic;
+         clk   : in std_logic);
+   end component;
+
+   procedure spiReadWord (
+      constant addr : in  natural range 0 to 2**15-1;
+      signal sck    : out std_logic;
+      signal mosi   : out std_logic;
+      signal cs_n   : out std_logic;
+      signal clk    : in  std_logic);
+
+   procedure spiWriteWord (
+      signal addr : in  std_logic_vector(14 downto 0);
+      signal data : in  std_logic_vector(15 downto 0);
+      signal sck  : out std_logic;
+      signal mosi : out std_logic;
+      signal cs_n : out std_logic;
+      signal clk  : in  std_logic);
+
 end spislave_pkg;
 
 -------------------------------------------------------------------------------
+
+package body spislave_pkg is
+
+   procedure spiReadWord (
+      constant addr : in  natural range 0 to 2**15-1;
+      signal sck    : out std_logic;
+      signal mosi   : out std_logic;
+      signal cs_n   : out std_logic;
+      signal clk    : in  std_logic) is
+      variable d : std_logic_vector(31 downto 0) := (others => '0');
+   begin
+      d(31 downto 16) := std_logic_vector(to_unsigned(addr, 16));
+
+      -- start 
+      cs_n <= '1';
+      sck  <= '0';
+      mosi <= '0';
+      wait for 50 ns;
+      cs_n <= '0';
+      wait for 100 ns;
+
+      -- 32 data bits
+      for ii in 31 downto 0 loop
+         sck  <= '0';
+         mosi <= d(ii);
+         wait for 50 ns;
+         sck  <= '1';
+         wait for 50 ns;
+      end loop;  -- ii
+
+      -- end
+      sck  <= '0';
+      wait for 50 ns;
+      cs_n <= '1';
+      sck  <= '0';
+      mosi <= 'Z';
+      wait for 100 ns;
+   end procedure;
+
+   procedure spiWriteWord (
+      signal addr : in  std_logic_vector(14 downto 0);
+      signal data : in  std_logic_vector(15 downto 0);
+      signal sck  : out std_logic;
+      signal mosi : out std_logic;
+      signal cs_n : out std_logic;
+      signal clk  : in  std_logic) is
+      variable d : std_logic_vector(31 downto 0) := (others => '0');
+   begin
+      d(31) := '1';                     -- MSB = '1' <=> write
+      d(30 downto 16) := addr(14 downto 0);
+      d(15 downto 0) := data;
+
+      -- start 
+      cs_n <= '1';
+      sck  <= '0';
+      mosi <= '0';
+      wait for 50 ns;
+      cs_n <= '0';
+      wait for 100 ns;
+
+      -- 32 data bits
+      for ii in 31 downto 0 loop
+         sck  <= '0';
+         mosi <= d(ii);
+         wait for 50 ns;
+         sck  <= '1';
+         wait for 50 ns;
+      end loop;  -- ii
+
+      -- end
+      sck  <= '0';
+      wait for 50 ns;
+      cs_n <= '1';
+      sck  <= '0';
+      mosi <= 'Z';
+      wait for 100 ns;
+   end procedure;
+
+end package body spislave_pkg;
--- fpga/modules/spislave/tb/spi_slave_tb.vhd
+++ fpga/modules/spislave/tb/spi_slave_tb.vhd
@@ -6,16 +6,12 @@
 -- Author     : cjt@users.sourceforge.net
 -- Company    : 
 -- Created    : 2011-07-31
--- Last update: 2012-04-29
+-- Last update: 2012-04-30
 -- Platform   : 
 -------------------------------------------------------------------------------
 -- Description: 
 -------------------------------------------------------------------------------
 -- Copyright (c) 2011 
--------------------------------------------------------------------------------
--- Revisions  :
--- Date        Version  Author  Description
--- 2011-07-31  1.0      calle   Created
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -29,137 +25,131 @@ use work.bus_pkg.all;
 -------------------------------------------------------------------------------
 
 entity spi_slave_tb is
-  
+
 end spi_slave_tb;
 
 -------------------------------------------------------------------------------
 
 architecture tb of spi_slave_tb is
 
-  -- component ports
-  signal mosi  : std_logic;
-  signal miso  : std_logic;
-  signal sck   : std_logic;
-  signal csn   : std_logic;
-  signal reset : std_logic := '1';
-  signal clk   : std_logic := '1';
+   -- component ports
+   signal mosi  : std_logic;
+   signal miso  : std_logic;
+   signal sck   : std_logic;
+   signal csn   : std_logic;
+   signal reset : std_logic := '1';
+   signal clk   : std_logic := '1';
 
-  signal bus_o : busmaster_out_type;
-  signal bus_i : busmaster_in_type;
+   signal bus_o : busmaster_out_type;
+   signal bus_i : busmaster_in_type;
 
-  signal bus_data : unsigned(15 downto 0) := (others => '0');
+   signal bus_data : unsigned(15 downto 0) := (others => '0');
+
+   signal debug_addr : std_logic_vector(14 downto 0);
+   signal debug_data : std_logic_vector(15 downto 0);
+   
 
 begin  -- tb
 
-  DUT : spi_slave port map (
-    --ireg    => open,
-    --bit_cnt => open,
+   DUT : spi_slave port map (
+      --ireg    => open,
+      --bit_cnt => open,
 
-    miso_p => miso,
-    mosi_p => mosi,
-    sck_p  => sck,
-    csn_p  => csn,
+      miso_p => miso,
+      mosi_p => mosi,
+      sck_p  => sck,
+      csn_p  => csn,
 
-    bus_o   => bus_o,
-    bus_i   => bus_i,
+      bus_o => bus_o,
+      bus_i => bus_i,
 
-    reset => reset,
-    clk   => clk);
+      reset => reset,
+      clk   => clk);
 
-  -- clock generation
-  Clk <= not Clk after 5.0 ns;
+   -- clock generation
+   Clk <= not Clk after 5.0 ns;
 
-  -- Change the bus data to find out when exactly the bus is sampled
-  process (clk) is
-  begin  -- process
-     if rising_edge(clk) then           -- rising clock edge
-          bus_i.data <= std_logic_vector(bus_data);
-          bus_data <= bus_data + 1;
-     end if;
-  end process;
+   -- Change the bus data to find out when exactly the bus is sampled
+   process (clk) is
+   begin  -- process
+      if rising_edge(clk) then          -- rising clock edge
+         bus_i.data <= std_logic_vector(bus_data);
+         bus_data   <= bus_data + 1;
+      end if;
+   end process;
 
-  process
-  begin
-    wait for 25 ns;
-    reset <= '0';
-  end process;
+   process
+   begin
+      wait for 25 ns;
+      reset <= '0';
+   end process;
 
-  process
-    variable d : std_logic_vector(31 downto 0);
-    
-  begin
-    wait until (reset = '0');
-    csn  <= '1';
-    sck  <= '0';
-    mosi <= '0';
-    wait for 50 ns;
-    csn  <= '0';
-    wait for 100 ns;
-
-    -- read access to addr 0x7000 with 0x0000 as dummy data. 
-    d := X"70000000";
-
-    for i in 31 downto 0 loop
-      sck  <= '0';
-      mosi <= d(i);
-      wait for 50 ns;
-      sck  <= '1';
-      wait for 50 ns;
+   process
+      variable d : std_logic_vector(31 downto 0);
       
-    end loop;  -- i
-    sck  <= '0';
-    wait for 50 ns;
-    csn  <= '1';
-    sck  <= '0';
-    mosi <= 'Z';
+   begin
+      debug_addr <= std_logic_vector(to_unsigned(16#0ff#, 15));
+      debug_data <= x"fe35";
+      wait until (reset = '0');
 
-    wait for 50 ns;
-    csn <= '0';
-    wait for 50 ns;
+      -- read access to addr 0x7000 with 0x0000 as dummy data. 
+      spiReadWord(addr => 16#7000#, sck => sck, mosi => mosi, cs_n => csn, clk => clk);
 
-    -- write access to addr 0x00ff with data 0xfe35
-    d := X"80ff" & X"fe35";
+      -- write access to addr 0x00ff with data 0xfe35
+      spiWriteWord(addr => debug_addr, data => debug_data, sck => sck, mosi => mosi, cs_n => csn, clk => clk);
 
-    for i in 31 downto 0 loop
+      --
+      wait for 1 us;
+      d := X"8209" & X"cd43";
+
+      -- start 
+      csn <= '1';
       sck  <= '0';
-      mosi <= d(i);
+      mosi <= '0';
       wait for 50 ns;
-      sck  <= '1';
-      wait for 50 ns;
-      
-    end loop;  -- i
+      csn <= '0';
 
-    -- no pause between two transfers: 
-    if false then
+
+      for i in 31 downto 0 loop
+         sck  <= '0';
+         mosi <= d(i);
+         wait for 50 ns;
+         sck  <= '1';
+         wait for 50 ns;
+         
+      end loop;  -- i
+
+      -- no pause between two transfers: 
+      if false then
+         sck  <= '0';
+         wait for 50 ns;
+         csn  <= '1';
+         mosi <= 'Z';
+         wait for 50 ns;
+         csn  <= '0';
+         wait for 50 ns;
+      end if;
+
+      -- write access to addr 0xf0f with data 0x1234
+      d := X"8f0f" & X"1234";
+
+      for i in 31 downto 0 loop
+         sck  <= '0';
+         mosi <= d(i);
+         wait for 50 ns;
+         sck  <= '1';
+         wait for 50 ns;
+         
+      end loop;  -- i
+
       sck  <= '0';
       wait for 50 ns;
       csn  <= '1';
       mosi <= 'Z';
-      wait for 50 ns;
-      csn <= '0';
-      wait for 50 ns;
-    end if;
 
-    -- write access to addr 0xf0f with data 0x1234
-    d := X"8f0f" & X"1234";
 
-    for i in 31 downto 0 loop
-      sck  <= '0';
-      mosi <= d(i);
-      wait for 50 ns;
-      sck  <= '1';
-      wait for 50 ns;
       
-    end loop;  -- i
-
-    sck  <= '0';
-    wait for 50 ns;
-    csn  <= '1';
-    mosi <= 'Z';
-
-
-    
-  end process;
+   end process;
 
 end tb;
 
