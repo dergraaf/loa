febaf83d9138cfe7056a6c41c89d6e227d02b35a
strongly-typed <strongly-typed@nan>
2013-02-21 00:54:39 +0100

    Emacs VHDL mode update script updated and a bit more generic.

--- fpga/emacs_update.sh
+++ fpga/emacs_update.sh
@@ -1,11 +1,13 @@
-wget http://www.iis.ee.ethz.ch/~zimmi/emacs/vhdl-mode-3.33.28.tar.gz
-tar xzvf vhdl-mode-3.33.28.tar.gz
-cd vhdl-mode-3.33.28/
-sudo cp /usr/share/emacs/23.2/lisp/progmodes/vhdl-mode.elc /usr/share/emacs/23.2/lisp/progmodes/vhdl-mode.elc.bak
+VHDL_VERSION="3.34.2"
+EMACS_VERSION="23.3"
+
+wget http://www.iis.ee.ethz.ch/~zimmi/emacs/vhdl-mode-${VHDL_VERSION}.tar.gz
+tar xzvf vhdl-mode-${VHDL_VERSION}.tar.gz
+cd vhdl-mode-${VHDL_VERSION}/
+sudo cp /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/vhdl-mode.elc /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/vhdl-mode.elc.bak
 mv vhdl-mode.elc vhdl-mode.elc.bak
 emacs --batch --eval "(byte-compile-file \"vhdl-mode.el\")"
-sudo cp vhdl-mode.elc /usr/share/emacs/23.2/lisp/progmodes/
+sudo cp vhdl-mode.elc /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/
 cd ..
-rm -r vhdl-mode-3.33.28/
-rm vhdl-mode-3.33.28.tar.gz
-
+rm -r vhdl-mode-${VHDL_VERSION}/
+rm vhdl-mode-${VHDL_VERSION}.tar.gz
