34da22d8489c70968291e1b8d7c2364dd7f94416
strongly-typed <strongly-typed@nan>
2013-02-21 19:57:07 +0100

    [iMotor] Sending a message works.

--- fpga/modules/imotor/hdl/imotor_sender.vhd
+++ fpga/modules/imotor/hdl/imotor_sender.vhd
@@ -3,7 +3,10 @@
 -------------------------------------------------------------------------------
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
--- Description: Simple UART that sends parallel data serially.
+-- Description:
+--
+-- Endianess: Little (as it is the default of ARM)
+-- (Transmits lower byte first)
 -- -------------------------------------------------------------------------------
 -- Copyright (c) 2013 strongly-typed
 -------------------------------------------------------------------------------
@@ -20,8 +23,8 @@ use work.imotor_module_pkg.all;
 entity imotor_sender is
 
    generic (
-      DATA_WORDS : positive := 2;
-      DATA_WIDTH : positive := 16;
+      DATA_WORDS : positive                     := 2;
+      DATA_WIDTH : positive                     := 16;
       START_BYTE : std_logic_vector(7 downto 0) := x"50";
       END_BYTE   : std_logic_vector(7 downto 0) := x"A0"
 
@@ -48,18 +51,27 @@ architecture behavioural of imotor_sender is
    type imotor_sender_state_type is (
       IDLE,                             -- Idle state:
       START,                            -- Sending start byte
-      STATE1,                           -- State 1: 
-      STATE2                            -- State 2:
+      DATA,                             -- Sending data bytes
+      STOP                              -- Sending stop byte
       );
 
    type imotor_sender_type is record
-      state : imotor_sender_state_type;
+      state      : imotor_sender_state_type;
+      data_out   : std_logic_vector(7 downto 0);
+      start      : std_logic;
+      byte_count : integer range 0 to DATA_WORDS * 2;  -- ToDo: Make dependent
+                                                       -- from data width
    end record;
 
    -----------------------------------------------------------------------------
    -- Internal signal declarations
    -----------------------------------------------------------------------------
-   signal r, rin : imotor_sender_type := (state => IDLE);
+   signal r, rin : imotor_sender_type := (
+      state      => IDLE,
+      start      => '0',
+      data_out   => (others => '0'),
+      byte_count => 0
+      );
 
    -----------------------------------------------------------------------------
    -- Component declarations
@@ -71,6 +83,8 @@ begin  -- architecture behavourial
    ----------------------------------------------------------------------------
    -- Connections between ports and signals
    ----------------------------------------------------------------------------
+   data_out_p  <= r.data_out;
+   start_out_p <= r.start;
 
    ----------------------------------------------------------------------------
    -- Sequential part of finite state machine (FSM)
@@ -85,20 +99,56 @@ begin  -- architecture behavourial
    ----------------------------------------------------------------------------
    -- Combinatorial part of FSM
    ----------------------------------------------------------------------------
-   comb_proc : process(r, start_in_p)
+   comb_proc : process(busy_in_p, data_in_p(0)(7 downto 0), r, start_in_p)
       variable v : imotor_sender_type;
-      
+
    begin
       v := r;
 
       case r.state is
          when IDLE =>
             if start_in_p = '1' then
-               v.state := START;
+               -- Send Start Byte
+               v.state    := START;
+               v.start    := '1';
+               v.data_out := START_BYTE;
             end if;
-         when START => null;
-         when STATE1 => null;
-         when STATE2 => null;
+         when START =>
+            v.start := '0';
+            if busy_in_p = '0' then
+               -- Send Data
+               v.state      := DATA;
+               v.start      := '1';
+               v.byte_count := 0;
+
+               if v.byte_count mod 2 = 0 then
+                  v.data_out := data_in_p(v.byte_count / 2)(7 downto 0);
+               else
+                  v.data_out := data_in_p(v.byte_count / 2)(15 downto 8);
+               end if;
+            end if;
+         when DATA =>
+            v.start := '0';
+            if busy_in_p = '0' then
+               v.byte_count := v.byte_count + 1;
+               v.start      := '1';
+
+
+               if v.byte_count = DATA_WORDS * 2 then
+                  v.state    := STOP;
+                  v.data_out := END_BYTE;
+               elsif v.byte_count mod 2 = 0 then
+                  v.data_out := data_in_p(v.byte_count / 2)(7 downto 0);
+               else
+                  v.data_out := data_in_p(v.byte_count / 2)(15 downto 8);
+               end if;
+            end if;
+         when STOP =>
+            v.start := '0';
+            if busy_in_p = '0' then
+               -- Do not send more data
+               v.state := IDLE;
+            end if;
       end case;
 
       rin <= v;
--- fpga/modules/imotor/tb/imotor_sender_tb.vhd
+++ fpga/modules/imotor/tb/imotor_sender_tb.vhd
@@ -32,9 +32,15 @@ architecture behavourial of imotor_sender_tb is
    signal clk : std_logic := '1';
 
    signal start_message_s : std_logic := '0';
-   signal clock_tx        : std_logic := '0';
+   signal clock_tx_s      : std_logic := '0';
 
-   signal imotor_input_s : imotor_input_type(1 downto 0) := ("0000000011111111", "1111111100000000");
+   signal imotor_input_s : imotor_input_type(1 downto 0) := (x"0403", x"0201");
+
+   signal data_tx_s : std_logic_vector(7 downto 0);
+   
+   signal start_tx_s : std_logic;
+   signal busy_tx_s  : std_logic;
+   signal txd_out_s  : std_logic;
 
 begin  -- architecture behavourial
 
@@ -45,21 +51,36 @@ begin  -- architecture behavourial
          DATA_WORDS => 2,
          DATA_WIDTH => 8)
       port map (
-         data_in_p  => imotor_input_s,
---         data_out_p  => data_out_p,
--- start_out_p => start_out_p,
-         busy_in_p  => '0',
-         start_in_p => start_message_s,
-         clk        => clk);
+         data_in_p   => imotor_input_s,
+         data_out_p  => data_tx_s,
+         start_out_p => start_tx_s,
+         busy_in_p   => busy_tx_s,
+         start_in_p  => start_message_s,
+         clk         => clk);
 
    imotor_timer_1 : imotor_timer
       generic map (
          CLOCK          => 50E6,
-         BAUD           => 1E6,
-         SEND_FREQUENCY => 1E3)
+         BAUD           => 10E6,
+         SEND_FREQUENCY => 1E5)
       port map (
-         clock_tx_out_p => clock_tx,
-         clk            => clk);
+         clock_tx_out_p   => clock_tx_s,
+         clock_send_out_p => start_message_s,
+         clk              => clk);
+
+   imotor_uart_tx_1 : entity work.imotor_uart_tx
+      generic map (
+         START_BITS => 1,
+         DATA_BITS  => 8,
+         STOP_BITS  => 1,
+         PARITY     => None)
+      port map (
+         data_in_p     => data_tx_s,
+         start_in_p    => start_tx_s,
+         busy_out_p    => busy_tx_s,
+         txd_out_p     => txd_out_s,
+         clock_tx_in_p => clock_tx_s,
+         clk           => clk);
 
    -- clock generation
    clk <= not clk after 10 ns;
@@ -70,13 +91,6 @@ begin  -- architecture behavourial
       -- insert signal assignments here
 
       wait until clk = '1';
-      wait for 0.5 us;
-      start_message_s <= '1';
-      wait until clk = '1';
-      start_message_s <= '0';
-
-      wait for 10 us;
-      start_message_s <= '1';
 
       wait until false;
 
