dd28ab68d6d10d268e35247ae941063aa6cd3139
strongly-typed <strongly-typed@nan>
2013-02-24 22:12:41 +0100

    (Kind-of) Auto-detect of Emacs version.

--- fpga/emacs_update.sh
+++ fpga/emacs_update.sh
@@ -1,13 +1,16 @@
 VHDL_VERSION="3.34.2"
-EMACS_VERSION="23.3"
+
+# Path including the version number
+EMACS_PREFIX=`dirname /usr/share/emacs/??.?/lisp`
 
 wget http://www.iis.ee.ethz.ch/~zimmi/emacs/vhdl-mode-${VHDL_VERSION}.tar.gz
 tar xzvf vhdl-mode-${VHDL_VERSION}.tar.gz
 cd vhdl-mode-${VHDL_VERSION}/
-sudo cp /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/vhdl-mode.elc /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/vhdl-mode.elc.bak
+sudo cp ${EMACS_PREFIX}/lisp/progmodes/vhdl-mode.elc ${EMACS_PREFIX}/lisp/progmodes/vhdl-mode.elc.bak
 mv vhdl-mode.elc vhdl-mode.elc.bak
 emacs --batch --eval "(byte-compile-file \"vhdl-mode.el\")"
-sudo cp vhdl-mode.elc /usr/share/emacs/${EMACS_VERSION}/lisp/progmodes/
+sudo cp vhdl-mode.elc ${EMACS_PREFIX}/lisp/progmodes/
 cd ..
 rm -r vhdl-mode-${VHDL_VERSION}/
 rm vhdl-mode-${VHDL_VERSION}.tar.gz
+
