ec1b37e1801f702a1cea294db31cc97f603bebd1
strongly-typed <strongly-typed@nan>
2013-03-24 03:11:16 +0100

    Xilinx config files and GUI-less synthesis

--- fpga/toplevel/2013_big_drive/ise/make.sh
+++ fpga/toplevel/2013_big_drive/ise/make.sh
@@ -1,12 +1,12 @@
 # Synthesis
 xst -intstyle ise \
-    -ifn "/home/rca/software/loa/fpga/toplevel/2013_big_drive/ise/toplevel.xst" \
-    -ofn "/home/rca/software/loa/fpga/toplevel/2013_big_drive/ise/toplevel.syr"
+    -ifn "toplevel.xst" \
+    -ofn "toplevel.syr"
 
 # Translate
 ngdbuild -intstyle ise -dd _ngo -nt timestamp \
-    -uc /home/rca/software/loa/fpga/toplevel/2013_big_drive/board.ucf \
-    -uc /home/rca/software/loa/fpga/toplevel/2013_big_drive/carrier.ucf \
+    -uc board.ucf \
+    -uc carrier.ucf \
     -p xc6slx9-tqg144-3 toplevel.ngc toplevel.ngd
 
 # Map
@@ -14,17 +14,30 @@ map \
     -intstyle ise \
     -p xc6slx9-tqg144-3 -w -logic_opt off -ol high -t 1 -xt 0 \
     -register_duplication off \
-    -r 4 -global_opt off -mt off -ir off -pr off -lc off \
-    -power off -o toplevel_map.ncd toplevel.ngd toplevel.pcf
+    -r 4 \
+    -global_opt off \
+    -mt off \
+    -ir off \
+    -pr off \
+    -lc off \
+    -power off \
+    -o toplevel_map.ncd toplevel.ngd toplevel.pcf
 
 # Place and Route
-par -w -intstyle ise -ol high -mt off toplevel_map.ncd toplevel.ncd toplevel.pcf
+par \
+    -w -intstyle ise \
+    -ol high \
+    -mt off toplevel_map.ncd toplevel.ncd toplevel.pcf
 
 # Generate Post-Place & Route Static Timing".
-trce -intstyle ise -v 3 -s 3 -n 3 -fastpaths \
+trce \
+    -intstyle ise \
+    -v 3 -s 3 -n 3 -fastpaths \
     -xml toplevel.twx toplevel.ncd \
     -o toplevel.twr toplevel.pcf
 
 # Generate Programming File".
-bitgen -intstyle ise -f toplevel.ut toplevel.ncd
+bitgen \
+    -intstyle ise \
+    -f toplevel.ut toplevel.ncd
 
