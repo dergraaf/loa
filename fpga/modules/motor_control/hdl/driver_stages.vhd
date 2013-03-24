43abea72c2b7347d7b25436675e60744b82b5134
strongly-typed <strongly-typed@nan>
2013-03-24 17:02:07 +0100

    Convert 2012-style bridges to 2013-style halfbridges

--- fpga/modules/motor_control/hdl/driver_stages.vhd
+++ fpga/modules/motor_control/hdl/driver_stages.vhd
@@ -26,3 +26,56 @@ begin
    bldc_driver_stage_st.c.high  <= bldc_driver_stage.c.high;
    bldc_driver_stage_st.c.low_n <= not bldc_driver_stage.c.low;
 end structural;
+
+
+---------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.motor_control_pkg.all;
+
+-- Convert from PWM1/2 + shutdown interface to ST halfbridge
+entity dc_driver_stage_converter is
+   port (
+      pwm1_in_p                : in  std_logic;
+      pwm2_in_p                : in  std_logic;
+      sd_in_p                  : in  std_logic;
+      dc_driver_stage_st_out_p : out dc_driver_stage_st_type
+      );
+
+end dc_driver_stage_converter;
+
+architecture structural of dc_driver_stage_converter is
+
+begin
+   process (sd_in_p)
+   begin
+      if sd_in_p = '1' then
+         -- disable both
+         dc_driver_stage_st_out_p.a.high  <= '0';
+         dc_driver_stage_st_out_p.a.low_n <= '1';
+         dc_driver_stage_st_out_p.b.high  <= '0';
+         dc_driver_stage_st_out_p.b.low_n <= '1';
+      else
+         if pwm1_in_p = '0' then
+            dc_driver_stage_st_out_p.a.high  <= '0';
+            dc_driver_stage_st_out_p.a.low_n <= '0';
+         else
+            dc_driver_stage_st_out_p.a.high  <= '1';
+            dc_driver_stage_st_out_p.a.low_n <= '1';            
+         end if;
+
+         if pwm2_in_p = '0' then
+            dc_driver_stage_st_out_p.b.high  <= '0';
+            dc_driver_stage_st_out_p.b.low_n <= '0';
+         else
+            dc_driver_stage_st_out_p.b.high  <= '1';
+            dc_driver_stage_st_out_p.b.low_n <= '1';            
+         end if;
+         
+      end if;
+   end process;
+end structural;
