b3651f14ae3c1d787945f0a6f9c23143dee80d65
strongly-typed <strongly-typed@nan>
2013-03-26 18:36:37 +0100

    Revert commit: SPI Mode 0 fix and add documentation about black magic.

--- fpga/modules/spislave/hdl/spislave.vhd
+++ fpga/modules/spislave/hdl/spislave.vhd
@@ -91,7 +91,6 @@ begin
 
       variable v                       : spi_slave_state_type;
       variable rising_sck, falling_csn : std_logic;
-      variable falling_sck : std_logic;
       
    begin
       v := r;
@@ -102,7 +101,6 @@ begin
 
       rising_sck  := v.sck(1) and not v.sck(2);
       falling_csn := v.csn(2) and not v.csn(1);
-      falling_sck := v.sck(2) and not v.sck(1);
 
       v.bus_we   := '0';
       v.bus_re   := '0';
@@ -136,10 +134,7 @@ begin
                   v.bus_we   := '1';
                   v.state    := WR;
                end if;
-            end if;
 
-            -- Update output at falling edge of SCK
-            if falling_sck = '1' then
                if not (v.bit_cnt = 0) then
                   v.bit_cnt := v.bit_cnt - 1;
                end if;
