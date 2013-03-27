4914a9ad53097c06eb57d27c60e0cd6cc301ef40
strongly-typed <strongly-typed@nan>
2013-03-27 01:16:33 +0100

    Add error checks to make script

--- fpga/toplevel/2013_big_drive/make.sh
+++ fpga/toplevel/2013_big_drive/make.sh
@@ -10,6 +10,11 @@ xst -intstyle xflow \
     -ifn "toplevel.xst" \
     -ofn "toplevel.syr"
 
+if [ $? != 0 ]
+	then echo "error in xst command"
+	exit 1
+fi
+
 # Translate
 ngdbuild \
 	-intstyle xflow \
@@ -18,6 +23,11 @@ ngdbuild \
     -uc ../board.ucf \
     -uc ../carrier.ucf \
     -p xc6slx9-tqg144-3 toplevel.ngc toplevel.ngd
+
+if [ $? != 0 ]
+	then echo "error in ngdbuild command"
+	exit 1
+fi
 
 # Map
 map \
@@ -37,6 +47,10 @@ map \
     -power off \
     -o toplevel_map.ncd \
 	toplevel.ngd toplevel.pcf
+if [ $? != 0 ]
+	then echo "error in map command"
+	exit 1
+fi
 
 # Place and Route
 par \
@@ -45,6 +59,10 @@ par \
     -ol high \
     -mt off \
 	toplevel_map.ncd toplevel.ncd toplevel.pcf
+if [ $? != 0 ]
+	then echo "error in par command"
+	exit 1
+fi
 
 # Generate Post-Place & Route Static Timing".
 trce \
@@ -52,6 +70,10 @@ trce \
     -v 3 -s 3 -n 3 -fastpaths \
     -xml toplevel.twx toplevel.ncd \
     -o toplevel.twr toplevel.pcf
+if [ $? != 0 ]
+	then echo "error in trce command"
+	exit 1
+fi
 
 # Generate Programming File".
 # needs toplevel.ut
