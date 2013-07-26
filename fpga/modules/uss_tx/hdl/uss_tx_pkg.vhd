d79fb3ada58975ceebfe39cba1bc87c69c762f3a
strongly-typed <strongly-typed@nan>
2013-07-26 22:19:46 +0200

    Remove modulation input from US TX module.
    Replaced by internal pattern serialiser.

--- fpga/modules/uss_tx/hdl/uss_tx_pkg.vhd
+++ fpga/modules/uss_tx/hdl/uss_tx_pkg.vhd
@@ -28,7 +28,6 @@ package uss_tx_pkg is
       uss_tx0_out_p    : out half_bridge_type;
       uss_tx1_out_p    : out half_bridge_type;
       uss_tx2_out_p    : out half_bridge_type;
-      modulation_p     : in  std_logic_vector(2 downto 0);
       clk_uss_enable_p : out std_logic;
       bus_o            : out busdevice_out_type;
       bus_i            : in  busdevice_in_type;
