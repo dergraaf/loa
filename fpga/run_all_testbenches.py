329c065e13152b197779a12131b6f3a20982ec72
strongly-typed <strongly-typed@nan>
2013-09-30 17:36:16 +0200

    Display results of test benches

--- fpga/run_all_testbenches.py
+++ fpga/run_all_testbenches.py
@@ -2,11 +2,33 @@
 
 import os
 
+# Populate list with items 
+L = []
 for r,d,f in os.walk("."):
     for file in f:
         if file.endswith("_tb.vhd"):
-            print 'Found', file, 'in', r
             testbench = file.rstrip(".vhd")
-            cmd = 'cd %s; make TESTBENCH="%s"' % (r, testbench)
-            os.system(cmd)
+            L.append( [r, testbench, None] )
+
+# Run testbenches
+i = iter(L)
+while True:
+    try:
+        item = i.next()
+    except StopIteration:
+        break
+
+    (dir, testbench, ret) = item
+
+    cmd = 'cd %s; make TESTBENCH="%s"' % (dir, testbench)
+    print "**** In %s running testbench %s" % (dir, testbench)
+    ret = os.system(cmd)
+    print "**** Return code is %d" % (ret)
+    print 
+    item[2] = ret
+    
+# Print results summary
+for dir, testbench, ret in L:
+    print '%-40s ==> %-40s ==> %3d' % (dir, testbench, ret) 
+
 
