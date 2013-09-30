e07ca32a303da43ca90e75d16d6c7d8b7f751071
strongly-typed <strongly-typed@nan>
2013-09-30 16:49:54 +0200

    Move test bench to tb directory

--- fpga/modules/motor_control/tb/dc_driver_stage_converter_tb.vhd
+++ fpga/modules/motor_control/tb/dc_driver_stage_converter_tb.vhd
@@ -2,13 +2,21 @@
 -- Title      : Testbench for design "dc_driver_stage_converter"
 -- Project    : 
 -------------------------------------------------------------------------------
--- File       : dc_driver_stage_converter_tb.vhd
+-- File       : dc_driver_stage_converter_tb.vhd<2>
+-- Author     : Sascha  <sascha@95-087.eduroam.rwth-aachen.de>
+-- Company    : 
+-- Created    : 2013-03-27
+-- Last update: 2013-03-27
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
 -- Description: 
 -------------------------------------------------------------------------------
--- Copyright (c) 2013 strongly-typed
+-- Copyright (c) 2013 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2013-03-27  1.0      sascha	Created
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -28,9 +36,9 @@ end entity dc_driver_stage_converter_tb;
 architecture tb of dc_driver_stage_converter_tb is
 
    -- component ports
-   signal pwm1_in_p                : std_logic := '0';
-   signal pwm2_in_p                : std_logic := '0';
-   signal sd_in_p                  : std_logic := '1';
+   signal pwm1_in_p                : std_logic;
+   signal pwm2_in_p                : std_logic;
+   signal sd_in_p                  : std_logic;
    signal dc_driver_stage_st_out_p : dc_driver_stage_st_type;
 
    -- clock
@@ -39,7 +47,7 @@ architecture tb of dc_driver_stage_converter_tb is
 begin  -- architecture tb
 
    -- component instantiation
-   DUT : entity work.dc_driver_stage_converter
+   DUT: entity work.dc_driver_stage_converter
       port map (
          pwm1_in_p                => pwm1_in_p,
          pwm2_in_p                => pwm2_in_p,
@@ -47,47 +55,17 @@ begin  -- architecture tb
          dc_driver_stage_st_out_p => dc_driver_stage_st_out_p);
 
    -- clock generation
-   clk <= not clk after 10 ns;
+   Clk <= not Clk after 10 ns;
 
    -- waveform generation
-   WaveGen_Proc : process
+   WaveGen_Proc: process
    begin
       -- insert signal assignments here
-
-      wait until clk = '1';
-
-      -- Deactivate
-      pwm1_in_p <= '0';
-      pwm2_in_p <= '0';
-      sd_in_p   <= '1';
-
-      -- Both low
-      wait until clk = '1';
-      pwm1_in_p <= '0';
-      pwm2_in_p <= '0';
-      sd_in_p   <= '0';
-
-      -- Both high
-      wait until clk = '1';
-      pwm1_in_p <= '1';
-      pwm2_in_p <= '1';
-      sd_in_p   <= '0';
-
-      -- High, low
-      wait until clk = '1';
-      pwm1_in_p <= '1';
-      pwm2_in_p <= '0';
-      sd_in_p   <= '0';
-
-      -- Low, High
-      wait until clk = '1';
-      pwm1_in_p <= '0';
-      pwm2_in_p <= '1';
-      sd_in_p   <= '0';
-
+      
+      wait until Clk = '1';
    end process WaveGen_Proc;
 
-
+   
 
 end architecture tb;
 
